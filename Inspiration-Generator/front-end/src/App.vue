1c24a3af3c3ef5e71a1d20adb86e65c42a296d47
Talin Kretchmer <zifle75@gmail.com>
2021-06-17 14:23:18 -0600

    Front end is kind of where I want it for now

---
+++ Inspiration-Generator/front-end/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <header id="header">
+      <h1>The Inspiration Generator</h1>
+      <nav>
+	<router-link to="/"><i class="fas fa-home"></i></router-link>
+	<router-link to="/dashboard"><i class="fas fa-user"></i></router-link>
+      </nav>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+
+<style>
+
+body {
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 300;
+  font-size: 13pt;
+  margin: 0px 100px;
+}
+
+#header {
+  /* Hopefully a Rectangle */
+  height:125px;
+  width: 100%;
+
+  /* Fixed Position */
+  position: fixed;
+  z-index: 10000;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  /* Color and Alignment */
+  background: #000000;
+  text-align: center;
+  box-shadow: 0 0 0 1em #FFF;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+h1 {
+  color: #fff;
+  font-size: 30px;
+}
+
+h2 {
+  font-size: 18px;
+}
+
+#header .fas {
+  font-size: 25px;
+  color: #fff;
+  width: 50px;
+}
+
+.pure-button-primary {
+  background-color: #277E8E;
+}
+
+
+</style>
---
+++ Inspiration-Generator/front-end/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import blank from blank
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
