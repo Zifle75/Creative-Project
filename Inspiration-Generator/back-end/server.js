a24c96a2c3a81319efb9dfd072df1418dabe2940
Talin Kretchmer <zifle75@gmail.com>
2021-06-17 18:13:46 -0600

    Progress! Photo compatability and style has been added

--- Inspiration-Generator/back-end/server.js
+++ Inspiration-Generator/back-end/server.js
@@ -36,5 +36,10 @@ const users = require("./users.js");
 app.use("/api/users", users.routes);
 
 
+// import the photos module and setup its API path
+const photos = require("./photos.js");
+app.use("/api/photos", photos.routes);
+
+
 app.listen(3002, () => console.log('Server listening on port 3002!'));
 
--- Inspiration-Generator/front-end/src/components/MyStuff.vue
+++ Inspiration-Generator/front-end/src/components/MyStuff.vue
@@ -1,25 +1,51 @@
 <template>
 <div class="main">
   <div class="menu">
-    <p><a><i class="fas fa-image"></i></a></p>
+    <h2><a @click="toggleUpload"><i class="fas fa-image"></i></a> Upload a Photo</h2>
+    <h2><a><i class="far fa-file-alt"></i></a> Upload Inspiration</h2>
+
     <h2>{{user.firstName}} {{user.lastName}} <a @click="logout"><i class="fas fa-sign-out-alt"></i></a></h2>
+    <uploader :show="show" @close="close" @uploadFinished="uploadFinished" />
   </div>
+  <image-gallery :photos="photos"/>
+  <p v-if="error">{{error}}</p>
 </div>
+
 </template>
 
 
 <script>
 import axios from 'axios';
+import Uploader from '@/components/Uploader.vue';
+import ImageGallery from '@/components/ImageGallery.vue';
+
 export default {
   name: 'MyStuff',
-  data() {
-    return {}
+  components: {
+    Uploader,
+    ImageGallery
   },
+
+  data() {
+    return {
+      show: false,
+      photos: [],
+      error: '',
+    }
+  },
+
+  created() {
+    this.getPhotos();
+  },
+
+
   computed: {
     user() {
       return this.$root.$data.user;
     },
   },
+
+
   methods: {
     async logout() {
       try {
@@ -29,12 +55,39 @@ export default {
         this.$root.$data.user = null;
       }
     },
+
+    async getPhotos() {
+      try {
+        this.response = await axios.get("/api/photos");
+        this.photos = this.response.data;
+      } catch (error) {
+        this.error = error.response.data.message;
+      }
+    },
+
+    close() {
+      this.show = false;
+    },
+
+    toggleUpload() {
+      this.show = true;
+    },
+
+    async uploadFinished() {
+      this.show = false;
+      this.getPhotos();
+    },
+
   }
 }
 </script>
 
 
 <style scoped>
+.main {
+  margin-top: 125px;
+}
+
 .menu {
   display: flex;
   justify-content: space-between;
